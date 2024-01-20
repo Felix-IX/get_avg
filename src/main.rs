bb4674b3ad40cff66541e180d4fa27e4997be2fa
Felix-IX <akafelixab@outlook.com>
2024-01-20 21:22:55 +0800

    polished main.rs
    use vector instead of array to store the user's input,
    allowed the program to calculate average of multiple numbers

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,43 @@
 use std::io;
 
 fn main() {
-        println!("type 3 numbers and the program will calculate the average");
-        let i: [f64; 3] = get_input();
-        let avg = cal_avg(i);
+        println!("input numbers and the program will calculate the average");
+        println!("enter 'z' to stop input");
+        let v: Vec<f64> = get_input();
+        let avg = cal_avg(v);
         println!("the average is: {}", avg);
 }
 
 //* This function prompts the user to enter three numbers and returns them as a fixed-size array.
-fn get_input() -> [f64; 3] {
-        let mut input_array: [f64; 3] = [0.0; 3];
-        for i in 0..3 {
-                println!("please enter the {}{} number", i + 1, ordinal_suffix(i + 1));
+fn get_input() -> Vec<f64> {
+        let mut i = 0;
+        let mut input_array: Vec<f64> = Vec::new();
+        loop {
+                println!("please enter the {}{} number, or z to stop", i + 1, ordinal_suffix(i));
 
                 let mut input_line = String::new();
                 io::stdin().read_line(&mut input_line).expect("failed to read line");
 
+                if &input_line.trim().to_lowercase()[..] == "z" {
+                        return input_array;
+                }
                 match input_line.trim().parse::<f64>() {
-                        Ok(num) => input_array[i] = num,
+                        Ok(num) => input_array.push(num),
                         Err(_) => panic!("invalid input"),
                 }
+                i += 1;
         }
-        input_array
 }
 
 //* A helper function that calculates the average of an array containing three floating-point numbers.
-fn cal_avg(ls: [f64; 3]) -> f64 {
-        let avg: f64 = (ls[0] + ls[1] + ls[2]) / 3.0;
+fn cal_avg(v: Vec<f64>) -> f64 {
+        let avg: f64 = v.iter().sum::<f64>() / v.len() as f64;
         avg
 }
 
 //* A helper function provides the suffix of the number
 fn ordinal_suffix(num: usize) -> &'static str {
-        match num % 10 {
+        match (num + 1) % 10 {
                 1 => "st",
                 2 => "nd",
                 3 => "rd",
