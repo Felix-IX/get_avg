74af52ee53891492f668762c4fb583d101ee044f
Felix-IX <akafelixab@outlook.com>
2024-01-21 13:47:02 +0800

    polished again and again ++

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,34 @@ fn main() {
         println!("Input numbers and the program will calculate the average");
         
         let v: Vec<f64> = get_input();
-        if check_not_empty(&v) {
-                let avg = cal_avg(v);
-                println!("The average is: {}", avg);
+        let avg = match v.len() {
+                0 => {
+                        println!("Please enter at least one number");
+                        return;
+                }
+                _ => cal_avg(v)
+        };
+        println!("The average is: {}", avg);
+
+}
+
+//* this function reads a number from the user and returns a tuple of (bool, bool) 
+//* the first value is whether the user entered a valid number, 
+//* the second value is whether the user entered "q" to quit.
+fn read_number(input_line: &str, input_vec: &mut Vec<f64>) -> (bool, bool) {
+        let trimmed = input_line.trim();
+        if trimmed == "q" {
+                return(false, true)
         }
-        else {
-                println!("Please enter at least one number")
+
+        match trimmed.parse::<f64>() {
+                Ok(num) => {
+                        input_vec.push(num);
+                        (true, false)
+                },
+                Err(_) => {
+                        (false, false)
+                },
         }
 }
 
@@ -21,20 +43,16 @@ fn get_input() -> Vec<f64> {
                 println!("Please enter the {}{} number, or q to stop", i + 1, ordinal_suffix(i));
 
                 let mut input_line = String::new();
-                match io::stdin().read_line(&mut input_line) {
-                        Ok(_) => {
-                                let trimmed = input_line.trim();
-                                if trimmed == "q" {
-                                        return input_vec;
-                                }
-                                match trimmed.parse::<f64>(){
-                                        Ok(num) => {input_vec.push(num);
-                                        i += 1;
-                                },
-                                        Err(_) => println!("Invalid number, did you mean q?")
-                                }
-                        }
-                        Err(_) => panic!("Failed to read line"),
+                io::stdin().read_line(&mut input_line).expect("Failed to read line");
+                
+                if read_number(&input_line, &mut input_vec) == (false, true) {
+                        return input_vec;
+                }
+                else if read_number(&input_line, &mut input_vec) == (true, false) {
+                        i += 1;
+                }
+                else if read_number(&input_line, &mut input_vec) == (false, false) {
+                        println!("Invalid number, did you mean q?\n");
                 }
         }
 }
@@ -43,11 +61,6 @@ fn get_input() -> Vec<f64> {
 fn cal_avg(v: Vec<f64>) -> f64 {
         let avg: f64 = v.iter().sum::<f64>() / v.len() as f64;
         avg
-}
-
-//* A helper function checks if there's at least a number in the vector
-fn check_not_empty(v: &Vec<f64>) -> bool {
-        !v.is_empty()
 }
 
 //* A helper function provides the suffix of the number
